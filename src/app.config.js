11958ae773e7dc1890be4232a6d399a2291494ed
shruthikrish <skrishy411@gmail.com>
2021-05-20 16:32:26 +0000

    First change

--- src/app.config.js
+++ src/app.config.js
@@ -4,7 +4,7 @@
  * DO NOT store state here - must be part of a stateless service
  */
 class AppConfig {
-    team = "Shruthi's Team";
+    team = "Shruthi's";
     version = '1.0.0';
 }
 module.exports = AppConfig;
